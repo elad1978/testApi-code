c4d7ea623a87622cafce1ae1d2d2ed112cd986a6
elad1978 <80212214+elad1978@users.noreply.github.com>
2023-06-25 10:50:36 +0300

    Update index.js

--- testApi-code/index.js
+++ testApi-code/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const jsonParser = bodyParser.json();
-const jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");//not in use
 
 const app = express();
 const PORT = 8070;
